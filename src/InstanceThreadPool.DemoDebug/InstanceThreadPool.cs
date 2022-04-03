beea010eb46a6f43fab03530bcbbff55c8dc3bca
LabsWebApp <vl.skopin@gmail.com>
2022-04-03 10:00:34 +0700

    Заготовка действий и блокирующих событий, обеспечивающих передачу заданий пулу

--- src/InstanceThreadPool.DemoDebug/InstanceThreadPool.cs
+++ src/InstanceThreadPool.DemoDebug/InstanceThreadPool.cs
@@ -6,6 +6,10 @@ public class InstanceThreadPool
     private readonly Thread[] _threads;
     private readonly ThreadPriority _priority;
 
+    private readonly Queue<(Action<object?> work, object? parameter)> _works = new();
+    private readonly AutoResetEvent _workingEvent = new(false);
+    private readonly AutoResetEvent _executeEvent = new(true);
+
     public string Name => _name ?? GetHashCode().ToString("x");
 
     public InstanceThreadPool(int maxThreadsCount, ThreadPriority priority = ThreadPriority.Normal, string? name = null)
@@ -39,7 +43,15 @@ public class InstanceThreadPool
 
     public void Execute(Action<object?> work, object? parameter)
     {
+        // запрашиваем доступ к очереди
+        _executeEvent.WaitOne();
 
+        // добавляем в очередь действие 
+        _works.Enqueue((work, parameter));
+        // разрешаем доступ к очереди
+        _executeEvent.Set();
+        
+        _workingEvent.Set();
     }
 
     private void WorkingThread()
